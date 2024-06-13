751b754760a72bf5cf04b244b05b5e1a313ac49d
SistemasGEP2 <sistemas2@gep.com.co>
2024-06-13 15:39:23 -0500

    Final

---
+++ api/views.py
@@ -0,0 +1,59 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from random import randrange
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def get_chart(_request):
+    
+    colors = ['blue', 'orange', 'red', 'black', 'yellow', 'green', 'magenta', 'lightblue', 'purple', 'brown']
+    random_colors = colors[(randrange(0,(len(colors)-1)))]
+    
+    serie = []
+    counter = 0
+    
+    
+    while (counter<7):
+        serie.append(randrange(100,400))
+        counter += 1
+        
+    chart = {
+        'tooltip' : {
+            'show' : True,
+            'trigger' : "axis",
+            'triggerOn' : "mousemove|click"
+            
+        },
+        'xAxis': [
+        {
+            'type':"category",
+            "data" : ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]   
+        }   
+        ],
+        'yAxis' : [
+            {
+                'type' : "value"
+            }
+        ],
+        'series' : [
+            {
+                'data' : serie,
+                'type' : "line",
+                'itemStyle' : {
+                    'color':random_colors
+                },
+                'lineStyle':{
+                    'color':random_colors
+                }
+            }
+        ]
+    }
+
+    return JsonResponse(chart)
+
+
